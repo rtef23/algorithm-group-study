0c6efd6ce2d39aa9a29583cc543de50549ea579f
maruoov <zmaru123@gmail.com>
2018-07-14 18:25:34 +0900

    1주차 문제풀이

---
+++ simminwoo/question1.cpp
@@ -0,0 +1,34 @@
+﻿#include <cstdio>
+using namespace std;
+
+int testNum;
+int input[10001];
+int orderedMultiplication[10001];
+int reverseMultiplication[10001];
+int result[10001];
+
+int main(void) {
+	scanf("%d", &testNum);
+
+	for (int i = 0; i < testNum; i++) {
+		scanf("%d", &input[i]);
+	}
+
+	int temp = 1;
+	int i;
+	for (i = 0; i < testNum; i++) {
+		orderedMultiplication[i] = temp;
+		temp *= input[i];
+	}
+
+	temp = 1;
+	for (int i = testNum - 1; i >= 0; i--) {
+		reverseMultiplication[i] = temp;
+		temp *= input[i];
+	}
+
+	for (i = 0; i < testNum; i++) {
+		result[i] = orderedMultiplication[i] * reverseMultiplication[i];
+		printf("%d ", result[i]);
+	}
+}
---
+++ simminwoo/question2.cpp
@@ -0,0 +1,58 @@
+﻿#include <cstdio>
+#include <queue>
+#include <string>
+using namespace std;
+
+int width, height;
+int map[10001][10001];
+int visit[10001][10001];
+int dx[4] = { -1, 0, 0, 1 };
+int dy[4] = { 0, 1, -1, 0 };
+int startX, startY;
+int endX, endY;
+
+int main(void)
+{
+	scanf("%d %d", &width, &height);
+
+	int i, j;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+
+	scanf("%d %d", &startX, &startY);
+	scanf("%d %d", &endX, &endY);
+
+	memset(visit, -1, sizeof(visit));
+	queue<pair<int, int> > currentPosition;
+	currentPosition.push(make_pair(startX, startY));
+	visit[startX][startY] = 0;
+
+	while (!currentPosition.empty()) {
+		int currentX = currentPosition.front().first;
+		int currentY = currentPosition.front().second;
+		int nextX, nextY;
+
+		currentPosition.pop();
+
+		if (currentX == endX && currentY == endY) {
+			continue;
+		}
+
+		for (int d = 0; d < 4; d++) {
+			nextX = currentX + dx[d];
+			nextY = currentY + dy[d];
+
+			if (nextX >= 0 && nextX < height && nextY >= 0 && nextY < width) {
+				if (map[nextX][nextY] == 1 && (visit[nextX][nextY] == -1 || visit[nextX][nextY] > visit[currentX][currentY])) {
+					visit[nextX][nextY] = visit[currentX][currentY] + 1;
+					currentPosition.push(make_pair(nextX, nextY));
+				}
+			}
+		}
+	}
+
+	printf("%d\n", visit[endX][endY]);
+}
