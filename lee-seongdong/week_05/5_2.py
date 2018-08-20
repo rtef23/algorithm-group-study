61cdd20ce552cabcf098bfa2cffa35dd2072697e
lee-seongdong <lee-seongdong@nts-corp.com>
2018-08-20 11:56:49 +0900

    [LSD] week5 문제풀이

--- lee-seongdong/week_05/5_2.py
+++ lee-seongdong/week_05/5_2.py
@@ -6,11 +6,8 @@ sys.stdin = open('../input.txt')
 for t in range(int(input())):
 	p_str = input().replace('?', '\w{1}').replace('*', '\w*')
 	p = re.compile('^' + p_str + '$')
+	n = int(input())
 
-	words = list()
-	for _ in range(int(input())):
-		words.append(input())
-
-	for word in sorted(filter(lambda x: p.match(x), words)):
+	for word in sorted(filter(p.match, [input() for _ in range(n)])):
 		print(word)
 
